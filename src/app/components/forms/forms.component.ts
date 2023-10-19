248f2466dca442dec29f45c10346a98add659a7d
Felipe Barras <felipeu_santos@hotmail.com>
2023-10-19 00:22:37 -0300

    nao esta diferenciando manha tarde e noite

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -3,6 +3,23 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { GetUnitsService } from 'src/app/services/get-available-units.service';
 import { Location } from 'src/app/types/location.interface';
 
+const OPENING_HOURS = {
+  morning: {
+    first: '06',
+    last: '12',
+  },
+  afternoon: {
+    first: '12',
+    last: '18',
+  },
+  evening: {
+    first: '18',
+    last: '23',
+  },
+};
+
+type HOUR_INDEXES = 'morning' | 'afternoon' | 'evening';
+
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
@@ -30,15 +47,74 @@ export class FormsComponent implements OnInit {
     });
   }
 
+  transformWeekday(weekday: number) {
+    switch (weekday) {
+      case 0:
+        return 'Dom.';
+      case 6:
+        return 'Sáb.';
+      default:
+        return 'Seg. à Sex.';
+    }
+  }
+
+  filterUnits(unit: Location, opened_hour: string, closed_hour: string) {
+    // como nem todas as unidades possuem calendários específicos (schedules), retorne true caso n tenha
+    if (!unit.schedules) return true;
+    let opened_hour_filter = parseInt(opened_hour, 10);
+    let closed_hour_filter = parseInt(closed_hour, 10);
+
+    let today_weekday = this.transformWeekday(new Date().getDay());
+
+    for (let i = 0; i < unit.schedules.length; i++) {
+      let schedule_hour = unit.schedules[i].hour;
+      let schedule_weekday = unit.schedules[i].weekdays;
+
+      if (today_weekday === schedule_weekday) {
+        if (schedule_hour !== 'Fechada') {
+          let [unit_open_hour, unit_close_hour] = schedule_hour.split(' às ');
+          let unit_open_hour_int = parseInt(
+            unit_open_hour.replace('h', ''),
+            10
+          );
+          let unit_close_hour_int = parseInt(
+            unit_open_hour.replace('h', ''),
+            10
+          );
+
+          if (
+            unit_open_hour_int <= opened_hour_filter &&
+            unit_close_hour_int >= closed_hour_filter
+          )
+            return true;
+          else return false;
+        }
+      }
+    }
+    return false;
+  }
+
   onSubmit(): void {
+    let intermediateResults = this.results;
+
     // momento que ocorrerá o filtro das unidades
     // depois de ter filtrado uma vez, precisa desfazer o que já foi feito
     if (!this.formGroup.value.showClosed) {
-      this.filteredResults = this.results.filter(
+      intermediateResults = this.filteredResults = this.results.filter(
         (location) => location.opened === true
       );
+    }
+
+    if (this.formGroup.value.hour) {
+      const OPENED_HOUR =
+        OPENING_HOURS[this.formGroup.value.hour as HOUR_INDEXES].first;
+      const CLOSED_HOUR =
+        OPENING_HOURS[this.formGroup.value.hour as HOUR_INDEXES].last;
+      this.filteredResults = intermediateResults.filter((location) =>
+        this.filterUnits(location, OPENED_HOUR, CLOSED_HOUR)
+      );
     } else {
-      this.filteredResults = this.results;
+      this.filteredResults = intermediateResults;
     }
   }
 
